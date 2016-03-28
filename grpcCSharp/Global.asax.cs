1839189b34caabf64a9f39e9cc1018235971a562
golamkibria <kibria_net@yahoo.com>
2016-03-28 17:24:51 +0600

    Added files via upload

---
+++ grpcCSharp/Global.asax.cs
@@ -0,0 +1,12 @@
+﻿namespace KNet
+{
+    using System.Web;
+
+    public class WebApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GrpcInitializer.EnsureGrpcCSharpExtNativeDll();
+        }
+    }
+}
